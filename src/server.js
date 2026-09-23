fcd3b3cbf7d124baa81ad9b0285edb8a4a0cff5f
bolajeee <ibrahimomobolaji1999@gmail.com>
2026-09-23 17:08:43 +0100

    log(2026-09-23): plain NodeJs and DSA - Valid anagram

--- src/server.js
+++ src/server.js
@@ -1,1 +1,34 @@
-console.log("Week 2 Node server starting...");
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+  if (request.method === "POST" && request.url === "/tasks") {
+    let body = "";
+
+    request.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    request.on("end", () => {
+
+      const task = JSON.parse(body);
+
+      console.log("Task:", task);
+      console.log("Title:", task.title);
+
+    response.end("Task received");
+  
+  });
+
+    return;
+  }
+
+  if (request.method === "GET" && request.url === "/tasks") {
+    response.end("Here are the tasks");
+    return;
+  }
+
+  response.statusCode = 404;
+  response.end("Not Found");
+});
+
+server.listen(4000);
